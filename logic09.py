f3902601c29c0813bc0c39a0629e4bda5b7dbff6
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-29 22:14:00 +0500

    log09-01

--- logic09.py
+++ logic09.py
@@ -7,4 +7,8 @@ def main(a,b):
     Returns:
         True if at least one of the numbers 'a' and 'b' is odd, False otherwise
     """
-    return
+    a%=2
+    b%=2
+    return a==1 or b==1
+x=main(5,6)
+print(x)
