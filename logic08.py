028c218c7be20021ff77b484d9427fee91927d60
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-29 22:13:34 +0500

    log08-01

--- logic08.py
+++ logic08.py
@@ -7,4 +7,8 @@ def main(a,b):
     Returns:
         True if at least one of the numbers 'a' and 'b' is even, False otherwise
     """
-    return
+    a%=2
+    b%=2
+    return a==0 or b==0
+x=main(5,6)
+print(x)
