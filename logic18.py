f417f35a9bb13bff6cc00f507026936a20275cf5
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-29 22:18:00 +0500

    log18-1

--- logic18.py
+++ logic18.py
@@ -5,4 +5,18 @@ def main(a):
     Returns:
         True if all digits of a are in descending order, False otherwise
     """
-    return
+    a1=a%10
+    a//=10
+
+    a2=a%10
+    a//=10
+
+    a3=a%10
+    a//=10
+
+    a4=a%10
+    a5=a//10    
+    print(a5,a4,a3,a2,a1)
+    return a5>a4>a3>a2>a1
+x=main(97643)
+print(x)
