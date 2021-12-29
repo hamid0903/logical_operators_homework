e10c5d527d63ef09792604f790783d0fe33f51f7
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-29 22:14:26 +0500

    log10-1

--- logic10.py
+++ logic10.py
@@ -6,4 +6,16 @@ def main(a):
     Returns:
         True if a is two-digit number, False otherwise
     """
-    return
+    x1=pow(0,a)
+    a//=10
+    x2=pow(0,a)
+    a//=10
+    x3=pow(0,a)
+    a//=10
+    x4=pow(0,a)
+    a//=10
+    x5=pow(0,a)
+    a=5-(x1+x2+x3+x4+x5)
+    return a==2
+x=main(255)
+print(x)
