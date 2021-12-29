d9d0fbe34be45e35d6be38e03284b95d2fc82f93
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-29 22:17:30 +0500

    log17-1

--- logic17.py
+++ logic17.py
@@ -6,4 +6,18 @@ def main(a):
     Returns:
         True if all digits of a are in ascending order, False otherwise
     """
-    return
+    a1=a%10
+    a//=10
+
+    a2=a%10
+    a//=10
+
+    a3=a%10
+    a//=10
+
+    a4=a%10
+    a5=a//10    
+    print(a5,a4,a3,a2,a1)
+    return a5<a4<a3<a2<a1
+x=main(12645)
+print(x)
